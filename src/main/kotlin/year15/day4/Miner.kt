e725ebfd740f6561e1f59443a1c496fe0525c379
Will-McBurney <pmcburn1@gmail.com>
2023-12-01 21:45:48 -0500

    Completed 2015 Day 4

---
+++ src/main/kotlin/year15/day4/Miner.kt
@@ -0,0 +1,28 @@
+package year15.day4
+
+import java.math.BigInteger
+import java.security.MessageDigest
+
+
+val myKey = "ckczppom"
+
+fun main() {
+    val startTime = System.currentTimeMillis();
+    for (i in 1..Integer.MAX_VALUE) {
+        md5Check(myKey, i, startTime)
+    }
+    val endTime = System.currentTimeMillis();
+
+}
+
+private fun md5Check(myKey: String, i: Int, startTime: Long) {
+    val input = myKey + i.toString()
+    val md = MessageDigest.getInstance("MD5")
+    val result = md.digest(input.toByteArray())
+    val bigInt = BigInteger(1, result)
+    val fullString = bigInt.toString(16).padStart(32, '0')
+    if (fullString.startsWith("000000")) {
+        val endTime = System.currentTimeMillis();
+        println("$i - Time Elapse: ${endTime - startTime}ms")
+    }
+}
