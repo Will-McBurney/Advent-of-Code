e82207be22b861e9871c4f44befeab28a2dd9e4c
WillMcBurney <pmcburn1@gmail.com>
2024-02-20 13:11:40 -0500

    Added 2021 day 1 and 2 - both parts

---
+++ src/main/kotlin/year21/day02/Main.kt
@@ -0,0 +1,37 @@
+package year21.day02
+
+import AoCResultPrinter
+import Reader
+const val year: Int = 23
+const val day: Int = 1
+
+
+
+fun main() {
+    val printer = AoCResultPrinter(year, day)
+
+    //Setup
+    val inputFilename = "test_input.txt"
+    val lines = Reader.getLines(year, day, inputFilename)
+
+    printer.endSetup()
+
+    //Do Part 1
+    val part1Result = getPart1Result()
+    printer.endPart1()
+
+    //Do Part 2
+    val part2Result = getPart2Result()
+    printer.endPart2()
+
+    //Display output
+    printer.printResults(part1Result, part2Result)
+}
+
+fun getPart1Result(): Int {
+    return 0
+}
+
+fun getPart2Result(): Int {
+    return 0
+}
