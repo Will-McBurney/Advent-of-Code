8053590c5e393f850081a62a6b2d2eb6ab811f8d
Will-McBurney <pmcburn1@gmail.com>
2024-12-01 00:33:47 -0500

    2024 Day 1 cleaned up

--- src/main/kotlin/year24/day01/Main.kt
+++ src/main/kotlin/year24/day01/Main.kt
@@ -16,37 +16,39 @@ fun main() {
     val inputFilename = "input.txt"
     val lines = Reader.getLines(year, day, inputFilename)
 
-    val l1 = mutableListOf<Int>()
-    val l2 = mutableListOf<Int>()
-    val pairs = lines.map{ it.split("\\s+".toRegex())}
-        .forEach { pair ->
-            l1.add(pair[0].toInt())
-            l2.add(pair[1].toInt())
-        }
+    val list1 = ArrayList<Int>(lines.size)
+    val list2 = ArrayList<Int>(lines.size)
 
-    val l1s = l1.sorted()
-    val l2s = l2.sorted()
+    // split lines on whitespace and add to sorted lists
+    lines.map{ it.split("\\s+".toRegex())}
+        .forEachIndexed { index, pair ->
+            list1.add(pair[0].toInt())
+            list2.add(pair[1].toInt())
+        }
+    val list1Sorted = list1.sorted()
+    val list2Sorted = list2.sorted()
 
     printer.endSetup()
 
     //Do Part 1
-    val part1Result = getPart1Result(l1s, l2s)
+    val part1Result = getPart1Result(list1Sorted, list2Sorted)
     printer.endPart1()
 
     //Do Part 2
-    val part2Result = getPart2Result(l1s, l2s)
+    val part2Result = getPart2Result(list1Sorted, list2Sorted)
     printer.endPart2()
 
     //Display output
     printer.printResults(part1Result, part2Result)
 }
 
-fun getPart1Result(l1: List<Int>, l2: List<Int>): Int {
-    return l1.mapIndexed{index, _ -> abs(l1[index] - l2[index]) }.sum()
+fun getPart1Result(list1Sorted: List<Int>, list2Sorted: List<Int>): Int {
+    return list1Sorted.mapIndexed{ index, _ -> abs(list1Sorted[index] - list2Sorted[index]) }.sum()
 }
 
-fun getPart2Result(l1: List<Int>, l2: List<Int>): Int {
-    val countMap = l2.groupingBy { it }.eachCount()
+fun getPart2Result(list1Sorted: List<Int>, list2Sorted: List<Int>): Int {
+    val counts = Array<Int>(100000){0}
+    list2Sorted.forEach { counts[it]++ }
 
-    return l1.sumOf{ it * (countMap[it] ?: 0) }
+    return list1Sorted.sumOf{ it * counts[it] }
 }
