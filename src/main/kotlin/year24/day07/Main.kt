897c5b31da8884d737765f05e90765fb60e57ee2
Will-McBurney <pmcburn1@gmail.com>
2024-12-07 01:04:06 -0500

    Day 7: 2024 - clean-up and repetition reduction

--- src/main/kotlin/year24/day07/Main.kt
+++ src/main/kotlin/year24/day07/Main.kt
@@ -37,55 +37,57 @@ fun main() {
 
 fun getPart1Result(answers: List<Long>, operands: List<List<Long>>): Long {
     return answers.zip(operands)
-        .filter {it -> isPossible_P1(it.first, it.second)}
-        .sumOf {it.first}
+        .filter {it -> isPossible(
+            it.first,
+            it.second,
+            listOf(
+                Long::plus,
+                Long::times
+            )
+        )}
+        .sumOf{it.first}
 }
 
-fun isPossible_P1(answer: Long, operands: List<Long>): Boolean {
-    require(operands.isNotEmpty())
-    return isPossible_P1(
-        answer,
-        operands,
-        operands.first(),
-        1
-    )
+fun Long.concat(other: Long): Long {
+    return (this.toString() + other.toString()).toLong()
 }
-
-private fun isPossible_P1(answer: Long, operands: List<Long>, current: Long, index: Int): Boolean {
-    if (current == answer && index == operands.size) {
-        return true
-    }
-    if (current > answer.toLong()) {
-        return false
-    }
-    if (index >= operands.size) {
-        return false
-    }
-    val sum = current + operands[index]
-    val product = current * operands[index]
-    return (isPossible_P2(answer, operands, product, index + 1) ||
-            isPossible_P2(answer, operands, sum, index + 1))
-}
-
 
 fun getPart2Result(answers: List<Long>, operands: List<List<Long>>): Long {
     return answers.zip(operands)
-        .filter {it -> isPossible_P2(it.first, it.second)}
+        .filter {it -> isPossible(
+            it.first,
+            it.second,
+            listOf(
+                Long::plus,
+                Long::times,
+                Long::concat
+            )
+        )}
         .sumOf {it.first}
 }
 
-
-fun isPossible_P2(answer: Long, operands: List<Long>): Boolean {
+fun isPossible(
+    answer: Long,
+    operands: List<Long>,
+    operations: List<(Long, Long) -> Long>
+): Boolean {
     require(operands.isNotEmpty())
-    return isPossible_P2(
+    return isPossible(
         answer,
         operands,
+        operations,
         operands.first(),
         1
     )
 }
 
-private fun isPossible_P2(answer: Long, operands: List<Long>, current: Long, index: Int): Boolean {
+private fun isPossible(
+    answer: Long,
+    operands: List<Long>,
+    operations: List<(Long, Long) -> Long>,
+    current: Long,
+    index: Int,
+): Boolean {
     if (current == answer && index == operands.size) {
         return true
     }
@@ -95,10 +97,8 @@ private fun isPossible_P2(answer: Long, operands: List<Long>, current: Long, ind
     if (index >= operands.size) {
         return false
     }
-    val sum = current + operands[index]
-    val product = current * operands[index]
-    val concat = (current.toString() + operands[index].toString()).toLong()
-    return (isPossible_P2(answer, operands, concat, index + 1)) ||
-            isPossible_P2(answer, operands, product, index + 1) ||
-            isPossible_P2(answer, operands, sum, index + 1)
+    return operations.map { f -> f(current, operands[index])  }
+        .any { result ->  isPossible(
+            answer, operands, operations, result, index + 1
+        ) }
 }
