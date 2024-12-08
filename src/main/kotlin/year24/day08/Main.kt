c9a46794f23ec95ccf33e4a2bffbe617aa6b439b
Will-McBurney <pmcburn1@gmail.com>
2024-12-08 00:50:42 -0500

    Day 8: 2024 - slight further cleanup

--- src/main/kotlin/year24/day08/Main.kt
+++ src/main/kotlin/year24/day08/Main.kt
@@ -1,8 +1,11 @@
 package year24.day08
 
 import AoCResultPrinter
+import Grid
+import GridCoordinate
 import Reader
-const val year: Int = 25
+
+const val year: Int = 24
 const val day: Int = 8
 
 
@@ -11,27 +14,98 @@ fun main() {
     val printer = AoCResultPrinter(year, day)
 
     //Setup
-    val inputFilename = "test_input.txt"
+    val inputFilename = "input.txt"
     val lines = Reader.getLines(year, day, inputFilename)
+
+    val grid = Grid<Char>(lines.map { line -> line.toCharArray().toMutableList() }
+        .toMutableList())
+
+    val antennaGroups = grid.coordinates.filterNot{ grid.get(it) == '.' }
+        .groupByTo(mutableMapOf()) { grid.get(it) }
+
 
     printer.endSetup()
 
     //Do Part 1
-    val part1Result = getPart1Result()
+    val part1Result = getResult(antennaGroups, grid, ::getAntinodesPart1)
     printer.endPart1()
 
     //Do Part 2
-    val part2Result = getPart2Result()
+    val part2Result = getResult(antennaGroups, grid, ::getAntinodesPart2)
     printer.endPart2()
 
     //Display output
     printer.printResults(part1Result, part2Result)
 }
 
-fun getPart1Result(): Int {
-    return 0
+
+fun getResult(
+    antennaGroups: MutableMap<Char, MutableList<GridCoordinate>>,
+    grid: Grid<Char>,
+    getAntinodes: (GridCoordinate, GridCoordinate, Grid<Char>) -> List<GridCoordinate>
+): Int {
+    return antennaGroups.keys
+        .map() { getAntinodeList(antennaGroups[it]!!, grid, getAntinodes) }
+        .flatten()
+        .distinct()
+        .count()
 }
 
-fun getPart2Result(): Int {
-    return 0
+
+
+fun getAntinodeList(
+    coordinates: List<GridCoordinate>,
+    grid: Grid<Char>,
+    getAntinodes: (GridCoordinate, GridCoordinate, Grid<Char>) -> List<GridCoordinate>
+): List<GridCoordinate> {
+    val antinodes = mutableListOf<GridCoordinate>();
+    for (i in coordinates.indices) {
+        for (j in i + 1 until coordinates.size) {
+            antinodes += getAntinodes(
+                coordinates[i],
+                coordinates[j],
+                grid
+            )
+        }
+    }
+    return antinodes
 }
+
+fun getAntinodesPart1(
+    a: GridCoordinate, b: GridCoordinate, grid: Grid<Char>
+): List<GridCoordinate> {
+    val dRow = b.row - a.row
+    val dCol = b.col - a.col
+    val out = mutableListOf<GridCoordinate>(
+        GridCoordinate(a.row - dRow, a.col - dCol),
+        GridCoordinate(b.row + dRow, b.col + dCol)
+    ).filter { grid.isInBounds(it)  }
+    return out
+}
+
+fun getAntinodesPart2(
+    a: GridCoordinate,
+    b: GridCoordinate,
+    grid: Grid<Char>
+): List<GridCoordinate> {
+    val dRow = b.row - a.row
+    val dCol = b.col - a.col
+    val antinodes = mutableListOf<GridCoordinate>();
+    var aAntinode = a.copy()
+    while (grid.isInBounds(aAntinode)) {
+        antinodes.add(aAntinode)
+        aAntinode = GridCoordinate(aAntinode.row - dRow, aAntinode.col - dCol)
+    }
+
+    var bAntinode = b.copy()
+    while (grid.isInBounds(bAntinode)) {
+        antinodes.add(bAntinode)
+        bAntinode = GridCoordinate(bAntinode.row + dRow, bAntinode.col + dCol)
+    }
+
+    return antinodes.filter { grid.isInBounds(it)  }
+}
+
+
+
+
