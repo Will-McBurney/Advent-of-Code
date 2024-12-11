a8b0ccb2f672fcfcc80d9659fb5e354b21a1bd16
Will-McBurney <pmcburn1@gmail.com>
2024-12-11 00:37:39 -0500

    Day 11 - solved

--- src/main/kotlin/year24/day11/Main.kt
+++ src/main/kotlin/year24/day11/Main.kt
@@ -2,6 +2,7 @@ package year24.day11
 
 import AoCResultPrinter
 import Reader
+
 const val year: Int = 24
 const val day: Int = 11
 
@@ -11,27 +12,52 @@ fun main() {
     val printer = AoCResultPrinter(year, day)
 
     //Setup
-    val inputFilename = "test_input.txt"
-    val lines = Reader.getLines(year, day, inputFilename)
+    val inputFilename = "input.txt"
+    val stones = Reader.getLines(year, day, inputFilename)[0].trim()
+        .split(" ")
+        .map { it.toLong() }
 
     printer.endSetup()
 
     //Do Part 1
-    val part1Result = getPart1Result()
+    val part1Result = getStoneCount(stones.toList(), 25)
     printer.endPart1()
 
     //Do Part 2
-    val part2Result = getPart2Result()
+    val part2Result = getStoneCount(stones.toList(), 75)
     printer.endPart2()
 
     //Display output
     printer.printResults(part1Result, part2Result)
 }
 
-fun getPart1Result(): Int {
-    return 0
+fun processStone(stone: Long): List<Long> {
+    if (stone == 0L) return listOf(1L)
+    if (stone.toString().length % 2 == 0) {
+        val length = stone.toString().length
+        return listOf(
+            stone.toString().substring(0, length / 2).toLong(),
+            stone.toString().substring(length / 2).toLong(),
+        )
+    }
+    return listOf(stone * 2024L)
 }
 
-fun getPart2Result(): Int {
-    return 0
+fun getStoneCount(stones: List<Long>, blinks: Int): Long {
+    return stones.sumOf { getStoneCountHelper(it, blinks) }
+}
+
+// memo for getStoneCountHelper
+val cache = mutableMapOf<Pair<Long, Int>, Long>()
+
+fun getStoneCountHelper(stone: Long, blinksRemaining: Int): Long {
+    if (blinksRemaining == 0) {
+        return 1L
+    }
+    if (cache.containsKey(stone to blinksRemaining)) {
+        return cache[stone to blinksRemaining]!!
+    }
+    val results = processStone(stone).sumOf { getStoneCountHelper(it, blinksRemaining - 1) }
+    cache[stone to blinksRemaining] = results
+    return results
 }
