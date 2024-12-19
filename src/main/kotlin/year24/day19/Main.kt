34724382528eea6a0b072fa0c3de8eae7ebd9c90
Will-McBurney <pmcburn1@gmail.com>
2024-12-19 06:49:21 -0500

    Day 19 Day 1 and 2 - this was a day 19 problem?

--- src/main/kotlin/year24/day19/Main.kt
+++ src/main/kotlin/year24/day19/Main.kt
@@ -6,32 +6,79 @@ const val year: Int = 24
 const val day: Int = 19
 
 
+lateinit var towels: List<String>
+
+var cache = mutableMapOf<Int, Long>()
+
 
 fun main() {
     val printer = AoCResultPrinter(year, day)
 
     //Setup
-    val inputFilename = "test_input.txt"
+    val inputFilename = "input.txt"
     val lines = Reader.getLines(year, day, inputFilename)
+    towels = lines[0].split(", ").sortedBy { it.length }.reversed()
+
+    val patterns = lines.subList(2, lines.size).map { it.trim() }
 
     printer.endSetup()
 
+    val results = patterns.map {
+        cache = mutableMapOf<Int, Long>()
+        val result = search(it, 0)
+        result
+    }
+
+    val part1 = patterns.count {
+        secondaryCache = mutableMapOf<Int, Boolean>()
+        searchPart1(it, 0)
+    }
+
     //Do Part 1
-    val part1Result = getPart1Result()
+    val part1Result = getPart1Result(results)
     printer.endPart1()
 
     //Do Part 2
-    val part2Result = getPart2Result()
+    val part2Result = getPart2Result(results)
     printer.endPart2()
 
     //Display output
     printer.printResults(part1Result, part2Result)
 }
 
-fun getPart1Result(): Int {
-    return 0
+var secondaryCache = mutableMapOf<Int, Boolean>()
+
+fun searchPart1(target: String, startingIndex: Int): Boolean {
+    if (startingIndex == target.length) return true
+    if (secondaryCache.containsKey(startingIndex)) return false
+    towels.forEach { towel ->
+        if (target.substring(startingIndex).startsWith(towel) && searchPart1(target, startingIndex + towel.length)) {
+            return true
+        }
+    }
+    secondaryCache[startingIndex] = false
+    return false
 }
 
-fun getPart2Result(): Int {
-    return 0
+
+fun search(target: String, startingIndex: Int): Long {
+    if (startingIndex == target.length) return 1
+    if (cache.containsKey(startingIndex)) return cache[startingIndex]!!
+    val sum = towels.sumOf { towel ->
+        if (target.substring(startingIndex).startsWith(towel)) {
+            search(target, startingIndex + towel.length)
+        } else 0
+    }
+    cache[startingIndex] = sum
+    return sum
+}
+
+
+
+fun getPart1Result(results: List<Long>): Int {
+    return results.count { it > 0 }
+}
+
+fun getPart2Result(results: List<Long>): Long {
+    return results.sum()
 }
