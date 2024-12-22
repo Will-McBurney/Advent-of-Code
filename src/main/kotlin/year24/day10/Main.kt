2f66575542459d478faa2eba01e038de33f4afff
Will-McBurney <pmcburn1@gmail.com>
2024-12-22 12:01:33 -0500

    Day 22 part 1 and part 2 complete.

--- src/main/kotlin/year24/day10/Main.kt
+++ src/main/kotlin/year24/day10/Main.kt
@@ -12,31 +12,30 @@ const val year: Int = 24
 const val day: Int = 10
 
 
-
 fun main() {
     val printer = AoCResultPrinter(year, day)
-
+    
     //Setup
     val inputFilename = "input.txt"
     val lines = Reader.getLines(year, day, inputFilename)
-    val grid = Grid(lines.map{
+    val grid = Grid(lines.map {
         it.toCharArray().map { c -> c.digitToInt() }.toMutableList()
     }.toMutableList())
-
+    
     val trailHeads = grid.coordinates.filter { coordinate -> grid.get(coordinate) == 0 }
-
+    
     printer.endSetup()
-
+    
     //Do Part 1
     val trailResultsList = getTrailResults(grid, trailHeads)
-
-    val part1Result = trailResultsList.sumOf{ it.getPart1Result() }
+    
+    val part1Result = trailResultsList.sumOf { it.getPart1Result() }
     printer.endPart1()
-
+    
     //Do Part 2
-    val part2Result = trailResultsList.sumOf{ it.getPart2Result() }
+    val part2Result = trailResultsList.sumOf { it.getPart2Result() }
     printer.endPart2()
-
+    
     //Display output
     printer.printResults(part1Result, part2Result)
 }
@@ -44,18 +43,18 @@ fun main() {
 data class AscendingGridNode(
     val gridCoordinate: GridCoordinate,
     val grid: Grid<Int>,
-): SearchNode<GridCoordinate> {
+) : SearchNode<GridCoordinate> {
     override fun getNode(): GridCoordinate {
         return gridCoordinate
     }
-
+    
     override fun getNext(): List<AscendingGridNode> {
         val coordinateValue = grid.get(gridCoordinate)
         return grid.getCardinalNeighbors(gridCoordinate)
             .filter { grid.get(it) == coordinateValue + 1 }
             .map { AscendingGridNode(it, grid) }
     }
-
+    
 }
 
 const val TARGET_HEIGHT = 9
@@ -64,7 +63,7 @@ class TrailResults(
     private val grid: Grid<Int>,
     private val destinationsReached: MutableSet<GridCoordinate> = mutableSetOf(),
     private var destinationCount: Int = 0
-): SearchResult<Int> {
+) : SearchResult<Int> {
     override fun updateResult(node: SearchNode<*>) {
         require(node is AscendingGridNode)
         if (TARGET_HEIGHT == grid.get(node.gridCoordinate)) {
@@ -72,9 +71,9 @@ class TrailResults(
             destinationsReached.add(node.gridCoordinate)
         }
     }
-
+    
     fun getPart1Result() = destinationsReached.size
-
+    
     fun getPart2Result() = destinationCount
 }
 
--- src/main/kotlin/year24/day22/Main.kt
+++ src/main/kotlin/year24/day22/Main.kt
@@ -2,36 +2,103 @@ package year24.day22
 
 import AoCResultPrinter
 import Reader
+
 const val year: Int = 24
 const val day: Int = 22
 
 
+const val step1Multiplier = 64L // shift left 6
+const val step2Divisor = 32L // shift right 5
+const val step3Multiplier = 2024L
+const val pruneModuloMinusOne = 16777215L // mod 2^24
 
 fun main() {
     val printer = AoCResultPrinter(year, day)
-
+    
     //Setup
-    val inputFilename = "test_input.txt"
+    val inputFilename = "input.txt"
     val lines = Reader.getLines(year, day, inputFilename)
-
+    
+    val initSecretNumbers = lines.map { it.toLong() }
+    
     printer.endSetup()
-
+    
     //Do Part 1
-    val part1Result = getPart1Result()
+    val part1Result = getPart1Result(initSecretNumbers, 2000)
     printer.endPart1()
-
+    
     //Do Part 2
-    val part2Result = getPart2Result()
+    val part2Result = getPart2Result(initSecretNumbers, 2000)
     printer.endPart2()
-
+    
     //Display output
     printer.printResults(part1Result, part2Result)
 }
 
-fun getPart1Result(): Int {
-    return 0
+
+fun getPart1Result(initSecretNumbers: List<Long>, repetitions: Int): Long {
+    var secretNumbers = initSecretNumbers.toMutableList()
+    secretNumbers.parallelStream().mapToLong { sn ->
+        var currentNumber = sn
+        repeat(2000) {
+            currentNumber = getNextNumber(currentNumber)
+        }
+        currentNumber
+    }
+    return secretNumbers.map { it.toLong() }.sum()
 }
 
-fun getPart2Result(): Int {
-    return 0
+fun getPart2Result(initSecretNumbers: List<Long>, repetitions: Int): Int {
+    val sequenceMaps = initSecretNumbers.parallelStream().map {
+        getSequenceMap(it)
+    }.toList()
+    val sequenceSums = sequenceMaps.fold(mutableMapOf<Int, Int>()) { acc, item ->
+        item.entries.forEach { (key, value) ->
+            acc[key] = value + acc.getOrDefault(key, 0)
+        }
+        acc
+    }
+    val bestSequence = sequenceSums.maxBy { it.value }
+    println(bestSequence)
+    return bestSequence.value
+}
+
+fun getSequenceMap(startingNumber: Long):Map<Int, Int> {
+    val numberList = getNumberList(startingNumber)
+    val sequenceMap = mutableMapOf<Int, Int>()
+    numberList.windowed(5)
+        .map {
+            it.map { n -> (n % 10).toInt() }
+        }
+        .map {
+            it.windowed(2).map { it[1] - it[0] }.toList() to (it[4])
+        }
+        .forEach { (sequence, score) ->
+            val hash = hashSequence(sequence)
+            if (!sequenceMap.contains(hash))
+                sequenceMap[hash] = score
+        }
+    return sequenceMap
+}
+
+fun getNumberList(startingNumber: Long, sequenceLength: Int = 2000): List<Long> {
+    val numberList = ArrayList<Long>(2001)
+    numberList.add(startingNumber)
+    var currentNumber = startingNumber
+    repeat(sequenceLength) {
+        currentNumber = getNextNumber(currentNumber)
+        numberList.add(currentNumber)
+    }
+    return numberList
+}
+
+fun hashSequence(sequence: List<Int>): Int {
+    require(sequence.size == 4)
+    return (sequence[0] shl(15)) + (sequence[1] shl 10) + (sequence[2] shl 5) + (sequence[3])
+}
+
+fun getNextNumber(currentNumber: Long): Long {
+    var secretNumber = (currentNumber xor (currentNumber shl (6))) and (16777215)
+    secretNumber = (secretNumber xor (secretNumber shr (5))) and (16777215)
+    return (secretNumber xor (secretNumber shl (11))) and (16777215)
 }
