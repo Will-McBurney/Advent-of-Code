53902df960b26698a0a70a7c8f3a8f209d24f80b
Will-McBurney <pmcburn1@gmail.com>
2024-12-25 00:18:44 -0500

    Day 25, huh

--- src/main/kotlin/year24/day25/Main.kt
+++ src/main/kotlin/year24/day25/Main.kt
@@ -3,7 +3,7 @@ package year24.day25
 import AoCResultPrinter
 import Reader
 const val year: Int = 24
-const val day: Int = 24
+const val day: Int = 25
 
 
 
@@ -11,13 +11,31 @@ fun main() {
     val printer = AoCResultPrinter(year, day)
 
     //Setup
-    val inputFilename = "test_input.txt"
+    val inputFilename = "input.txt"
     val lines = Reader.getLines(year, day, inputFilename)
 
+    val keys = mutableListOf<List<Int>>()
+    val locks = mutableListOf<List<Int>>()
+    
+    lines.windowed(7, step = 8) { window ->
+        println(window)
+        if (window[0] == "#####") { // is lock
+            locks.add(
+                (0..4).map { index -> window.map { it[index] }.indexOf('.')-1 }
+            )
+        } else {
+            keys.add(
+                (0..4).map { index -> 6 - window.map { it[index] }.indexOf('#') }
+            )
+        }
+    }
+    println(locks)
+    println(keys)
+    
     printer.endSetup()
 
     //Do Part 1
-    val part1Result = getPart1Result()
+    val part1Result = getPart1Result(locks, keys)
     printer.endPart1()
 
     //Do Part 2
@@ -28,8 +46,12 @@ fun main() {
     printer.printResults(part1Result, part2Result)
 }
 
-fun getPart1Result(): Int {
-    return 0
+fun getPart1Result(locks: MutableList<List<Int>>, keys: MutableList<List<Int>>): Int {
+    return locks.sumOf { lock ->
+        keys.count { key ->
+            (0..4).all { key[it] + lock[it] <= 5 }
+        }
+    }
 }
 
 fun getPart2Result(): Int {
