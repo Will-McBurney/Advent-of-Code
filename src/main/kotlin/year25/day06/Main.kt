c23bd6e48fda76134da7c3b5275ae4934983fcb1
Will-McBurney <pmcburn1@gmail.com>
2025-12-06 02:47:05 -0500

    Day 6 Part 2 done. Code is ugly though

--- src/main/kotlin/year25/day06/Main.kt
+++ src/main/kotlin/year25/day06/Main.kt
@@ -23,6 +23,23 @@ fun main() {
     //Setup
     val inputFilename = "input.txt"
     val lines = Reader.getLines(year, day, inputFilename)
+   
+
+    printer.endSetup()
+
+    //Do Part 1
+    val part1Result = getPart1Result(lines)
+    printer.endPart1()
+
+    //Do Part 2
+    val part2Result = getPart2Result(lines)
+    printer.endPart2()
+
+    //Display output
+    printer.printResults(part1Result, part2Result)
+}
+
+fun getPart1Result(lines: List<String>): Long {
     val numProblems = lines.first().trim().replace("\\s+".toRegex(), " ").split(" ").size
     val problems = Array(numProblems){ MathProblem() }
     lines.subList(0, lines.size - 1).forEach { line ->
@@ -36,25 +53,41 @@ fun main() {
         .forEachIndexed { i, str ->
             problems[i].operation = str[0]
         }
-
-    printer.endSetup()
-
-    //Do Part 1
-    val part1Result = getPart1Result(problems)
-    printer.endPart1()
-
-    //Do Part 2
-    val part2Result = getPart2Result()
-    printer.endPart2()
-
-    //Display output
-    printer.printResults(part1Result, part2Result)
-}
-
-fun getPart1Result(problems: Array<MathProblem>): Long {
     return problems.sumOf { it.result }.also { println(it) }
 }
 
-fun getPart2Result(): Int {
-    return 0
+fun getPart2Result(lines: List<String>): Long {
+    val grid = lines.subList(0, lines.size - 1).map { line ->
+        line.toList()
+    }
+    
+    val columnSplits = grid.first().indices.filter { columnIndex ->
+        isColumnEmpty(columnIndex, grid)
+    } + grid.maxOf { it.size }
+    
+    val problems = Array(columnSplits.size){ MathProblem() }
+    
+    var startingColumn = 0
+    columnSplits.forEachIndexed { i, nextSplit ->
+        (startingColumn..< nextSplit).forEach { columnIndex ->
+            problems[i].numbers.add(getNumberFromColumn(columnIndex, grid))
+            startingColumn = nextSplit + 1
+        }
+    }
+    val operations = lines.last().trim().replace("\\s+".toRegex(), " ")
+        .split(" ")
+        .map{ it[0] }
+    operations.forEachIndexed { i, op -> problems[i].operation = op }
+    
+    return problems.sumOf { it.result }.also { println(it) }
+}
+
+fun isColumnEmpty(columnIndex: Int, grid: List<List<Char>>) =
+    grid.all { it[columnIndex] == ' ' }
+
+fun getNumberFromColumn(columnIndex: Int, grid: List<List<Char>>): Long {
+    return grid.indices.map { rowIndex -> grid[rowIndex][columnIndex] }
+        .joinToString("") { it.toString() }
+        .trim()
+        .toLong()
 }
