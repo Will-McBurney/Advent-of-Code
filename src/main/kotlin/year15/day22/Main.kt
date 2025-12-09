a3831b899d62e28d1d5951261bfa6dff9e2ed823
Will-McBurney <pmcburn1@gmail.com>
2025-12-09 12:14:04 -0500

    Further refactoring of Day 22, year 2015

--- src/main/kotlin/year15/day22/Main.kt
+++ src/main/kotlin/year15/day22/Main.kt
@@ -13,51 +13,80 @@ enum class Turn {
 }
 
 data class Node(
+    /** Player's remaining hp (uncapped) */
     val playerHp: Int,
+    /** Player's remaining mana (uncapped) */
     val playerMana: Int,
+    /** Boss's remaining hp */
     val bossHp: Int,
+    /** How many remaining phases [Effect.SHIELDED] is active. If 0, then [Effect.SHIELDED] is inactive*/
     val shieldedRemaining: Int = 0,
+    /** How many remaining phases [Effect.POISONED] is active. If 0, then [Effect.POISONED] is inactive*/
     val poisonedRemaining: Int = 0,
+    /** How many remaining phases [Effect.RECHARGING] is active. If 0, then [Effect.RECHARGING] is inactive*/
     val rechargingRemaining: Int = 0,
+    /** Whose [Turn] is it */
     val turn: Turn = Turn.PLAYER,
 )
 
-enum class Moves(
+/** Spells a player can cast on their turn */
+enum class Spells(
+    /** The mana cost of the move */
     val manaCost: Int,
+    /** The amount of **immediate** damage caused by the move */
     val damage: Int = 0,
+    /** The amount of **immediate** healing caused by the move */
     val heal: Int = 0,
+    /** The effect started by the move */
     val effect: Effect? = null,
-
     /** Condition that must be true to be allowed to cast, not counting mana cost*/
     val castCondition: (Node) -> Boolean = { _ -> true }
 ){
+    /** standard damage spell */
     MAGIC_MISSILE(manaCost = 53, damage = 4),
+    /** drain health from the boss */
     DRAIN(manaCost = 73, damage = 2, heal = 2),
+    /** increase armor for several turn */
     SHIELD(manaCost = 113, effect = Effect.SHIELDED, castCondition = { node -> node.shieldedRemaining <= 1 }),
+    /** deal damage over time to the boss for several turns */
     POISON(manaCost = 173, effect = Effect.POISONED, castCondition = { node -> node.poisonedRemaining <= 1 }),
+    /** regenerate mana over the next several turns */
     RECHARGE(manaCost = 229, effect = Effect.RECHARGING, castCondition = { node -> node.rechargingRemaining <= 1 }),
 }
 
+/**
+ * List of effects created by spells
+ */
 enum class Effect(
+    /** how many turns the effect lasts upon casting */
     val initialDuration: Int,
+    /** how much the effect increases player armor while the effect is active */
     val armorIncrease: Int = 0,
+    /** how much damage the spell does to the boss each turn while the effect is active */
     val damage: Int = 0,
+    /** how much mana the player gains each turn while the effect is active */
     val manaIncrease: Int = 0
 ) {
+    /** The player has increased defenses */
     SHIELDED(6, armorIncrease = 7),
+    /** The boss is taking damage each turn */
     POISONED(6, damage = 3 ),
+    /** The player is regenerating mana */
     RECHARGING(5, manaIncrease = 101);
 }
 
-enum class GameMode {
-    EASY,
+/** The game's difficult */
+enum class Difficulty {
+    /** Normal game difficulty */
+    NORMAL,
+    /** The player takes 1 additional point of damage on their turn */
     HARD
 }
 
 fun main() {
     val printer = AoCResultPrinter(year, day)
 
-    //Setup
+    // Get initial game state
     val startingPlayerHp = 50
     val startingPlayerMana = 500
     val inputFilename = "input.txt"
@@ -69,11 +98,23 @@ fun main() {
 
 
     //Do Part 1
-    val part1Result = getLeastManaSpent(startingPlayerHp, startingPlayerMana, startingBossHp, bossDamage, gameMode = GameMode.EASY)
+    val part1Result = getLeastManaSpent(
+        startingPlayerHp,
+        startingPlayerMana,
+        startingBossHp,
+        bossDamage,
+        Difficulty.NORMAL
+    )
     printer.endPart1()
 
     //Do Part 2
-    val part2Result = getLeastManaSpent(startingPlayerHp, startingPlayerMana, startingBossHp, bossDamage, gameMode = GameMode.HARD)
+    val part2Result = getLeastManaSpent(
+        startingPlayerHp,
+        startingPlayerMana,
+        startingBossHp,
+        bossDamage,
+        Difficulty.HARD
+    )
     printer.endPart2()
 
     //Display output
@@ -85,7 +126,7 @@ fun getLeastManaSpent(
     startingPlayerMana: Int,
     startingBossHp: Int,
     bossDamage: Int,
-    gameMode: GameMode
+    difficulty: Difficulty
 ): Int {
     val startingNode = Node(
         playerHp = startingPlayerHp,
@@ -103,7 +144,6 @@ fun getLeastManaSpent(
     // Searching
     while (!queue.isEmpty()) {
         val node = queue.poll()
-        //println("${bestCost[node]} $node")
 
         var playerHp = node.playerHp
         var playerMana = node.playerMana
@@ -112,9 +152,10 @@ fun getLeastManaSpent(
         var rechargingRemaining = node.rechargingRemaining
         var shieldedRemaining = node.shieldedRemaining
         val turn = node.turn
+        val manaSpent = bestCost[node]!!
 
         //apply hard mode penalty on player turn only
-        if (turn == Turn.PLAYER && gameMode == GameMode.HARD) {
+        if (turn == Turn.PLAYER && difficulty == Difficulty.HARD) {
             playerHp -= 1
             if (playerHp <= 0) continue // Player dead, kill branch
         }
@@ -152,9 +193,8 @@ fun getLeastManaSpent(
                 // if player is dead, this is a dead branch
                 if (playerHp <= 0) continue
 
-                // create new node
-                val nextNode = node.copy(
-                    //otherwise proceed to player turn
+                // proceed to player turn
+                val nextNode = Node(
                     playerHp = playerHp,
                     playerMana = playerMana,
                     bossHp = bossHp,
@@ -164,7 +204,7 @@ fun getLeastManaSpent(
                     rechargingRemaining = rechargingRemaining,
                 )
                 //Add if new node, or is a better cost than our old node
-                if (bestCost[nextNode] == null || bestCost[node]!! < bestCost[nextNode]!!) {
+                if (bestCost[nextNode] == null || manaSpent < bestCost[nextNode]!!) {
                     bestCost[nextNode] = bestCost[node]!!
                     queue.add(nextNode)
                 }
@@ -172,17 +212,15 @@ fun getLeastManaSpent(
 
             Turn.PLAYER -> { // handle player turn
                 // get the valid spell options
-                val playerOptions = Moves.entries.filter {
+                val playerOptions = Spells.entries.filter {
                     playerMana >= it.manaCost && it.castCondition(node)
                 }
 
-                if (playerOptions.isEmpty()) {
-                    continue
-                } // DEAD - cannot cast any moves
+                if (playerOptions.isEmpty()) { continue } // GAME OVER - cannot cast any moves
 
                 //for each spell, simulate the next phase, and create the new node
                 playerOptions.forEach { choice ->
-                    val nextNode = node.copy(
+                    val nextNode = Node(
                         playerHp = playerHp + choice.heal,
                         playerMana = playerMana - choice.manaCost,
                         bossHp = bossHp - choice.damage,
@@ -197,11 +235,11 @@ fun getLeastManaSpent(
                                 if (choice.effect == Effect.RECHARGING) choice.effect.initialDuration
                                 else 0
                     )
-                    val totalManaSpent = bestCost[node]!! + choice.manaCost
+                    val newManaSpent = manaSpent + choice.manaCost
 
                     // if this is a "never before seen" node, OR it is the best cost for this node
-                    if (bestCost[nextNode] == null || totalManaSpent < bestCost[nextNode]!!) {
-                        bestCost[nextNode] = totalManaSpent
+                    if (bestCost[nextNode] == null || newManaSpent < bestCost[nextNode]!!) {
+                        bestCost[nextNode] = newManaSpent
                         queue.add(nextNode)
                     }
                 }
