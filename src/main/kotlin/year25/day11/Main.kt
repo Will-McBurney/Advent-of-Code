f9ec31a655aee9f319a7090de967df948ebcf12a
Will-McBurney <pmcburn1@gmail.com>
2025-12-13 11:22:04 -0500

    Day 11 Part 2 Making some progress, but stopping for headache

--- src/main/kotlin/year25/day11/Main.kt
+++ src/main/kotlin/year25/day11/Main.kt
@@ -2,7 +2,6 @@ package year25.day11
 
 import AoCResultPrinter
 import Reader
-import java.util.*
 
 const val year: Int = 25
 const val day: Int = 11
@@ -39,34 +38,46 @@ fun main() {
     printer.printResults(part1Result, part2Result)
 }
 
-fun getPart1Result(servers: Map<String, Server>): Int {
-    val start = servers["you"]!!
-    val stack = Stack<Server>().apply { this.add(start) }
-    var outCount = 0
-    while (stack.isNotEmpty()) {
-        val node = stack.pop()
-        node.connections.forEach { str ->
-            if (str == "out") outCount++
-            else stack.add(servers[str]!!)
-        }
-    }
-    return outCount
+
+fun getPart1Result(servers: Map<String, Server>): Long {
+    val start = servers["svr"]!!
+    val p1Cache = mutableMapOf<Server, Long>()
+    return recursiveP1(start, servers, p1Cache)
 }
 
-fun getPart2Result(servers: Map<String, Server>): Int {
-    val start = servers["svr"]!!
-    val stack = Stack<List<Server>>().apply { this.add(listOf(start)) }
-    var outCount = 0
-    while (stack.isNotEmpty()) {
-        val node = stack.pop()
-        node.last().connections.forEach { str ->
-            if (str == "out") {
-                if (node.contains(servers["fft"]!!) && node.contains(servers["dac"]!!)) {
-                    outCount++
-                }
-            } else if (!node.contains(servers[str]))
-                stack.add(node + servers[str]!!)
-        }
+fun recursiveP1(server: Server, servers: Map<String, Server>, p1Cache: MutableMap<Server, Long>): Long {
+    if (p1Cache[server] != null) return p1Cache[server]!!
+    if (server.connections.contains("out")) {
+        p1Cache[server] == 1L
+        return 1L
     }
-    return outCount
+    p1Cache[server] = server.connections.sumOf { c ->
+        recursiveP1(servers[c]!!, servers, p1Cache)
+    }
+    return p1Cache[server]!!
+}
+
+
+data class P2Node(
+    val id: String,
+    val hasDac: Boolean,
+    val hasFft: Boolean
+)
+
+fun getPart2Result(servers: Map<String, Server>): Long {
+    val p2Cache = mutableMapOf<P2Node, Long>()
+    val start = P2Node("svr", false, false)
+    return recursiveP2(start, servers, p2Cache)
+}
+
+fun recursiveP2(node: P2Node, servers: Map<String, Server>, p2Cache: MutableMap<P2Node, Long>): Long {
+    if (p2Cache[node] != null) return p2Cache[node]!!
+    if (servers[node.id]!!.connections.contains("out")) {
+        p2Cache[node] == 1L
+        return 1L
+    }
+    p2Cache[node] = servers[node.id]!!.connections.sumOf { c ->
+        recursiveP2(node.copy(id = c), servers, p2Cache)
+    }
+    return p2Cache[node]!!
 }
